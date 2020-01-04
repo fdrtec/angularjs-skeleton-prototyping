9dce6db132c29ca68586e94eae5b60dac9630f4c
Fabiano Ramires <fdrtec@gmail.com>
2020-01-04 13:37:08 -0300

    feature-dev-1 sincronizando controller  do angularjs com html inicial

---
+++ app/appController.js
@@ -0,0 +1,12 @@
+angular
+    .module('app')
+    .controller('appController', Controller)
+
+Controller.$inject = [];
+
+function Controller() {
+    const self = this;
+
+    self.teste = "controller funcional"
+
+}
