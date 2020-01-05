ed6a3745aff31ddb7a623bbe8fd9ceffa90b78cb
Fabiano Ramires <fdrtec@gmail.com>
2020-01-05 19:15:40 -0300

    feature-dev-6 gulp funcional

---
+++ app/index.js
@@ -0,0 +1,8 @@
+angular
+    .module('app', [
+        'ui.router',
+        'ngAnimate',
+        'toastr',
+        'ngMaterial',
+        'ngMessages'
+    ]);
--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -6,7 +6,7 @@ const uglifycss = require('gulp-uglifycss');
 gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts']);
 
 gulp.task('deps.js', () => {
-    gulp.src([
+    return gulp.src([
         'node_modules/angular/angular.min.js',
         'node_modules/angular-material/angular-material.min.js',
         'node_modules/angular-animate/angular-animate.min.js',
@@ -14,25 +14,29 @@ gulp.task('deps.js', () => {
         'node_modules/angular-messages/angular-messages.min.js',
         'node_modules/angular-ui-router/release/angular-ui-router.min.js',
         'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
+        'node_modules/jquery/dist/jquery.min.js',
         'node_modules/bootstrap/dist/js/bootstrap.min.js'
+
     ])
         .pipe(uglify())
         .pipe(concat('deps.min.js'))
         .pipe(gulp.dest('public/assets/js'));
+
 });
 gulp.task('deps.css', () => {
-    gulp.src([
+    return gulp.src([
         'node_modules/angular-toastr/dist/angular-toastr.min.css',
         'node_modules/font-awesome/css/font-awesome.min.css',
         'node_modules/bootstrap/dist/css/bootstrap.min.css',
+
     ])
-        .pipe(uglifycss({'uglyComments':true}))
+        .pipe(uglifycss({'uglyComments': true}))
         .pipe(concat('deps.min.css'))
         .pipe(gulp.dest('public/assets/css'));
 
 });
 gulp.task('deps.fonts', () => {
-    gulp.src([
+    return gulp.src([
         'node_modules/font-awesome/fonts/*.*',
     ])
         .pipe(gulp.dest('public/assets/fonts'));
