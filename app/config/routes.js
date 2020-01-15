cd0fe07f95ba6b3bace1e3e0ffd53b25d595e4fc
Fabiano Ramires <fdrtec@gmail.com>
2020-01-15 10:11:04 -0300

    ft-router-1 primeira rota definida com template isolado e responsivo

---
+++ app/config/routes.js
@@ -0,0 +1,12 @@
+angular
+    .module('app')
+    .config(['$stateProvider', '$urlRouterProvider',
+        function ($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/');
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: '../templates/main.html'
+                })
+
+        }]);
